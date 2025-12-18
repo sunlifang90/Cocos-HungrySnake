7eeb549e36bfa09b7bd544bd584fed0ab1fa245c
sunlifang1990 <982290835@qq.com>
2025-12-18 22:47:52 +0800

    fix:优化身体创建方法，调整节点层级

--- assets/Scripts/Head.ts
+++ assets/Scripts/Head.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, instantiate, math, Node, Prefab, UITransform, v2, Vec2, Vec3 } from 'cc';
+import { _decorator, Collider2D, Component, Contact2DType, director, instantiate, math, Node, Prefab, UITransform, v2, Vec2, Vec3 } from 'cc';
 import { Joystick } from './Joystick';
 const { ccclass, property } = _decorator;
 
@@ -45,6 +45,10 @@ export class Head extends Component {
             this.moveBody();
         }, 0.3);
         this.creaeteFood();
+
+        // 获取碰撞器并启用碰撞检测
+        const headCollider = this.node.getComponent(Collider2D);
+        headCollider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
     }
 
     update(deltaTime: number) {
@@ -105,18 +109,61 @@ export class Head extends Component {
     private createBody() {
         const bodyNode = instantiate(this.bodyPrefab);
 
+        let vec:Vec3 = null;
+        let prevBodyNode:Node = null;
+        if (this.bodyList.length === 1) {
+            // 设置第一个身体节点的位置(以原点、蛇头向量为基准)
+            prevBodyNode = this.bodyList[0];
+            const headPosition = this.node.getPosition();
+            vec = headPosition.clone().normalize();
+        } else {
+            // 设置第二个之后身体节点的位置(以前一个身体节点位置、前前一个身体节点位置为基准)
+            // 获取前前一个身体节点的位置
+            const prevOneBodyNode = this.bodyList[this.bodyList.length - 2];
+            const prevOnePosition = prevOneBodyNode.getPosition();
+            // 获取前一个身体节点的位置
+            prevBodyNode = this.bodyList[this.bodyList.length - 1];
+            const prevPosition = prevBodyNode.getPosition();
+            //计算方向向量
+            vec = prevOnePosition.clone().subtract(prevPosition.clone()).normalize();
+        }
         // 获取前一个身体节点的位置
-        const prevBodyNode = this.bodyList[this.bodyList.length - 1];
         const prevPosition = prevBodyNode.getPosition();
         // 设置新身体节点的位置
-        bodyNode.setPosition(prevPosition.clone().subtract(prevPosition.clone().normalize().multiplyScalar(this.bodyDistance)));
+        bodyNode.setPosition(prevPosition.clone().add(vec.multiplyScalar(this.bodyDistance)));
         this.node.getParent().addChild(bodyNode);
         this.bodyList.push(bodyNode);
+        this.changeIndex();
     }
 
     private creaeteFood() {
         const foodNode =  instantiate(this.foodPrefab);
         this.node.getParent().addChild(foodNode);
+    }
+
+    private changeIndex() {
+        // 调整节点层级，使蛇头显示在最前面
+        
+        let index = this.node.getSiblingIndex();
+        let lastIndex = this.bodyList[this.bodyList.length - 1].getSiblingIndex();
+
+        let temp = this.bodyList[this.bodyList.length - 1];
+        temp.setSiblingIndex(index);
+        this.node.setSiblingIndex(lastIndex);
+    }
+
+    private onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D) {
+        if (otherCollider.group === 2) {
+            console.log('吃到食物');
+            // 销毁食物节点
+            otherCollider.node.destroy();
+            // 创建新的身体节点
+            this.createBody();
+            // 生成新的食物
+            this.creaeteFood();
+        } else if (otherCollider.group === 4) {
+            director.pause();
+        }
     }
 }
 
